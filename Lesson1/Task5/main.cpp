6a5d366980ed793baa0ae3f4f0aaabd0930371f5
vlad90435 <vlad90435@gmail.com>
2021-07-07 02:30:41 +0300

    Deleted junk int "result" and reworked code structure.

--- Lesson1/Task5/main.cpp
+++ Lesson1/Task5/main.cpp
@@ -5,30 +5,26 @@ using namespace std;
 int main() {
     int first_number = 0;
     int second_number = 0;
-    int result = 0;
     int action = 0;
     cout << "Vvedite pervoe chislo:\n";
     cin >> first_number;
     cout << "Vvedite vtoroe chislo:\n";
     cin >> second_number;
-    cout << "Vvedite deistvie 1-4 :\n";
+    cout << "Vvedite deistvie 1-4 :\n1 deistvie: summa chisel\n2 deistvie: raznitsa chisel \n3 deistvie umnojenie chisel \n4 destvie delenie chisel:"<<endl;
     cin >> action;
+    cout << "Resultat: ";
     switch (action) {
         case 1:
-            result = first_number + second_number;
-            cout << "Resultat:" << result;
+            cout << first_number + second_number;
             break;
         case 2:
-            result = first_number - second_number;
-            cout << "Resultat:" << result;
+            cout  << first_number - second_number;
             break;
         case 3:
-            result = first_number * second_number;
-            cout << "Resultat:" << result;
+            cout << first_number * second_number;
             break;
         case 4:
-            result = first_number / second_number;
-            cout << "Resultat:" << result;
+            cout  << first_number / second_number;
             break;
         default:
             cout << "Vy vveli nepravilnoe deistvie!";
