70565548f4ae79e9b6e3c037030bbe57264f9f95
vlad90435 <vlad90435@gmail.com>
2021-07-08 00:44:57 +0300

    Reworked Task 4

--- Lesson1/Task4/main.cpp
+++ Lesson1/Task4/main.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
+
 using namespace std;
 
 int main()
 {
     int salary=0;
-    cout << "Skolko ti zarabativaesh $$$ ?:\n";
+    cout << "Skolko ti zarabativaesh $$$:\n";
     cin >> salary;
-    if (salary>1000){
-        if (salary<1000000){
+    if ( salary < 1000 ) {
+        cout << "Rabotai bolshe,no ti molodec!";
+    }
+    if ( salary > 999 ) {
+        if ( salary > 999999 ) {
+            cout << "Ti millioner!";
+        }
+        if ( salary < 1000000 ) {
             cout << "Kruto!";
         }
-    }
-    if (salary<1000){
-        cout << "Rabotai boslhe!,no ti molodec";
-    }
-    if (salary>1000000){
-        cout << "Da ti millioner!";
     }
 }
