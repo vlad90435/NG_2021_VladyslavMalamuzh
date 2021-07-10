5b34228cf8a287e041e8182d04833cf70cc59436
vlad90435 <vlad90435@gmail.com>
2021-07-10 23:32:26 +0300

    Deleted my russian commit and added correct Task 4

--- Lesson1/Task4/main.cpp
+++ Lesson1/Task4/main.cpp
@@ -1,21 +1,22 @@
-#include <iostream>
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
-    int salary=0;
-    cout << "Skolko ti zarabativaesh $$$:\n";
-    cin >> salary;
-    if ( salary < 1000 ) {
-        cout << "Rabotai bolshe,no ti molodec!";
-    }
-    if ( salary > 999 ) {
-        if ( salary > 999999 ) {
-            cout << "Ti millioner!";
-        }
-        if ( salary < 1000000 ) {
-            cout << "Kruto!";
-        }
-    }
+    int salary=0;
+    cout << "Skolko ti zarabativaesh $$$:\n";
+    cin >> salary;
+    if ( salary < 1000 ) { 
+        cout << "Rabotai bolshe";
+    }
+    if ( salary > 999 ) { 
+        if ( salary > 999999 ) {
+            cout << "Ti millioner!";
+        }
+        if ( salary < 1000000 ) { 
+            cout << "Kruto!";
+        }
+    }
+    cout << ",no ti molodec!";
 }
