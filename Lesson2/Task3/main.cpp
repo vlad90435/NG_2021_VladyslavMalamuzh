4aec781994b509160e9ad8980c277da1924068a1
Vladyslav Malamuzh <vlad90435@gmail.com>
2021-07-12 23:44:00 +0300

    Added Task3

---
+++ Lesson2/Task3/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int square_size=0;
+    cout << "Enter square size :" << endl;
+    cin >> square_size;
+    for(int i=0;square_size>i;i++){
+        for(int j=0;square_size>j;j++){
+        cout << "*";
+        }
+        cout << "\n";
+    }
+}
