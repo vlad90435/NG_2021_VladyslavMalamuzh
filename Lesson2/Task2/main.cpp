271013bc438c7ab3c055f9a65ffb6245d82281d4
Vladyslav Malamuzh <vlad90435@gmail.com>
2021-07-13 15:04:42 +0300

    Code quality fix in loop for

--- Lesson2/Task2/main.cpp
+++ Lesson2/Task2/main.cpp
@@ -7,7 +7,7 @@ int main()
     int number=0;
     cout << "Enter number: ";
     cin >> number;
-    for(int i=0;i<number;i++){
+    for(int i=0; i < number; i++){
         cout << i << ", ";
     }
     cout << number;
