ef39df210b8e5c7441803253f3f73d87de8ea4a2
Vladyslav Malamuzh <vlad90435@gmail.com>
2021-07-13 12:55:19 +0300

    Changed square_size>i,j to i,j<square_size in loop for

--- Lesson2/Task3/main.cpp
+++ Lesson2/Task3/main.cpp
@@ -7,8 +7,8 @@ int main()
     int square_size=0;
     cout << "Enter square size :" << endl;
     cin >> square_size;
-    for(int i=0;square_size>i;i++){
-        for(int j=0;square_size>j;j++){
+    for(int i=0;i<square_size;i++){
+        for(int j=0;j<square_size;j++){
             cout << "*";
         }
         cout << endl;
