d4083d11276d250e25e2b66b46dbb606d64a0a46
Vladyslav Malamuzh <vlad90435@gmail.com>
2021-07-14 00:49:00 +0300

    Code quality fix

--- Lesson2/Task4/main.cpp
+++ Lesson2/Task4/main.cpp
@@ -9,7 +9,7 @@ int main()
     cin >> size;
     for (int i = 0; i <= size; i++){ // the cycle started with 0
         for (int j = 0; j < i; j++){ // The cycle started with the stage when i=1
-             cout << "*";
+            cout << "*";
         }
         cout << endl; // condition is satisfied as long as i<=size
     }
@@ -26,20 +26,19 @@ int main()
             cout << " "; // will output a blank starts after the first iteration of i
         }
         for (int k = 0; k < i; k++){ // is always performed until i is less than k
-        cout << "*";
+            cout << "*";
     }
         cout << endl; // is always true as long as i>0
     }
-    cout << endl;
     for (int i = 0; i <= size; i++){ // the cycle started with 0
-        for (int j = 0; j < size - i; j++){ 
+        for (int j = 0; j < size - i; j++){
             cout << " ";
             /* will print a blank, until j < size -1.
             on the first iteration of the cycle,the number of gaps will be maximum,then it will gradually decrease
             */
         }
         for (int k = 0; k < i; k++) // is always performed until i is less than k
-        cout << "*";
+            cout << "*";
         cout << endl;
     }
 }
