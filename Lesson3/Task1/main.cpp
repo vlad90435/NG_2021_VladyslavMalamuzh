b1cba73636e7bcb5885aa4ca3ef28d31b1d2071b
Vladyslav Malamuzh <vlad90435@gmail.com>
2021-07-21 22:32:08 +0300

    Deleted ru localisation,reworked project on english language

--- Lesson3/Task1/main.cpp
+++ Lesson3/Task1/main.cpp
@@ -4,17 +4,16 @@ using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL,"");
     short arr[10];
     short school = 0;
     bool school_search = 0;
 
     for (int school_fill = 0; school_fill < 10; school_fill++)
     {
-        cout << "Ўкола є [" << school_fill + 1 << "]:" << endl;
+        cout << "School # [" << school_fill + 1 << "]:" << endl;
         cin >> arr[school_fill];
     }
-    cout << "¬ведите номер вашей школы <3 :" << endl;
+    cout << "Enter number of your school <3 :" << endl;
     cin >> school;
     for (int school_find = 0; school_find < 10; school_find++)
     {
@@ -25,11 +24,11 @@ int main()
     }
     if (school_search == true)
     {
-        cout << "я знаю эту школу <3: " << endl;
+        cout << "I know this school <3: " << endl;
     }
     else
     {
-        cout << "я не знаю эту школу :( " << endl;
+        cout << "I don`t know this school :( " << endl;
     }
 }
 
