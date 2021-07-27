ffd0b1c7b94a978a4a64b3c77a5ab7d56a36cb51
Vladyslav Malamuzh <vlad90435@gmail.com>
2021-07-28 01:15:00 +0300

    Task 3 done

---
+++ Lesson4/Task3/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    char str[100];
+
+    cin.getline(str, 100);
+    while(str[i] != 0)
+    {
+        if(str[i]<='z' && str[i]>='a')
+        {
+            str[i]+='A'-'a';
+        }
+    cout << str[i];
+    i++;
+    }
+}
+    // Another method to do this task
+    /*for (int i = 0; str[i] != '\0'; i++){
+        if(str[i]<='z' && str[i]>='a')
+        {
+            str[i]+='A'-'a';
+        }
+    cout << str[i];
+    */
+
+
+
+
