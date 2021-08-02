63d0cd4d3ae952611661c4f4356fac7ecf3a66f2
vlad90435 <vlad90435@gmail.com>
2021-08-02 15:18:11 +0300

    Reworked Task3

--- Lesson4/Task3/main.cpp
+++ Lesson4/Task3/main.cpp
@@ -8,27 +8,13 @@ int main()
     char str[100];
 
     cin.getline(str, 100);
-    while(str[i] != 0)
+    for (int i = 0; str[i] != '\0'; i++)
     {
         if(str[i]<='z' && str[i]>='a')
         {
-            str[i]+='A'-'a';
+            str[i] -= 32;
         }
         cout << str[i];
-        i++;
     }
 }
-    // Another method to do this task
-    /*for (int i = 0; str[i] != '\0'; i++)
-    {
-        if(str[i]<='z' && str[i]>='a')
-        {
-            str[i]+='A'-'a';
-        }
-        cout << str[i];
-    }
-    */
-
-
-
 
