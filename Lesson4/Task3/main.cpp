e4ed838c9cfb1a752d85de6411d0a13e30f45d82
vlad90435 <vlad90435@gmail.com>
2021-08-06 00:08:22 +0300

    Added a description of the program for the user

--- Lesson4/Task3/main.cpp
+++ Lesson4/Task3/main.cpp
@@ -7,7 +7,10 @@ int main()
     int i = 0;
     char str[100];
 
+    cout << "Vvedite stroku,kotoruyu hotite preobrazovat v verhniy registr:" << endl;
     cin.getline(str, 100);
+
+
     for (int i = 0; str[i] != '\0'; i++)
     {
         if(str[i]<='z' && str[i]>='a')
