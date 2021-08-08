34a0bd68b3eb7bc6f292cb3b633455c5e67f648a
Vladyslav Malamuzh <vlad90435@gmail.com>
2021-08-08 23:17:53 +0300

    Deleted the extra char

--- Lesson4/Task3/main.cpp
+++ Lesson4/Task3/main.cpp
@@ -4,25 +4,20 @@ using namespace std;
 
 int main()
 {
-    
     char str[100];
-    char small_letter = 0;
-    
+
     cout << "Vvedite stroku,kotoruyu hotite preobrazovat v verhniy registr:" << endl;
     cin.getline(str, 100);
-
 
     for (int i = 0; str[i] != '\0'; i++)
     {
         if (str[i]<='z' && str[i]>='a')
         {
-            small_letter=str[i];
-            cout << (char) (small_letter-=32);
+            cout << (char) (str[i]-=32);
         }
         else
         {
-            cout << str[i];    
+            cout << str[i];
         }
-       
     }
 }
