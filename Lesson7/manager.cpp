417cda667664c9902b26cbe53b9849fa1c8c6e67
Vladyslav Malamuzh <vlad90435@gmail.com>
2021-08-26 23:39:34 +0300

    corrected the output in manager cpp. And replaced endl with n

--- Lesson7/manager.cpp
+++ Lesson7/manager.cpp
@@ -13,14 +13,17 @@ float manager::priceAll()
 
 void manager::getInfo()
 {
-    cout << "[CPU INFO]: " << endl << "\tNAME: " << cpu.getName() << endl << "\tCOMPANY: " << cpu.getCompany() << endl
-         << "\tARCHITECT: " << cpu.getArchitect() << endl << "\tHERTZ: " << cpu.getHertz() << endl
-         <<"\tPRICE: "<< cpu.getPrice()<< endl << "\tSOCKET: " << cpu.getSocket() << endl;
-    cout << "[VIDEOCARD INFO]: " << endl << "\tNAME: " << m_videocard.getName() << endl << "\tPRICE: " << m_videocard.getPrice() << endl
-         << "\tFREQUENCY: " << m_videocard.getFrequency() << endl << "\tSLOT: " << m_videocard.getSlot() << endl;
-    cout << "[MOTHERBOARD INFO]: " << endl << "\tNAME: " << m_motherboard.getName() << endl <<"\tCOMPANY: " << m_motherboard.getCompany() << endl
-         << "\tVIDEOSLOT: " << m_motherboard.getVideoSlot() << endl << "\tMEMORYSLOT: " << m_motherboard.getMemorySlot() << endl
-         << "\tPRICE: "<< m_motherboard.getPrice() << endl << "\tSOCKET: " << m_motherboard.getSocket() << endl;
-    cout << "Price for all: " << priceAll() << " UAH" << endl;
+    cout << "[CPU INFO]: \n " << "\tNAME: " << cpu.getName() << "\n" << "\tCOMPANY: " << cpu.getCompany() << "\n"
+         << "\tARCHITECT: " << cpu.getArchitect() << "\n" << "\tHERTZ: " << cpu.getHertz() << "\n"
+         << "\tPRICE: " << cpu.getPrice() << "\n" << "\tSOCKET: " << cpu.getSocket() << "\n";
+
+    cout << "[VIDEOCARD INFO]: \n" << "\tNAME: " << m_videocard.getName() << "\n" << "\tPRICE: " << m_videocard.getPrice() << "\n"
+         << "\tFREQUENCY: " << m_videocard.getFrequency() << "\n" << "\tSLOT: " << m_videocard.getSlot() << "\n";
+    
+    cout << "[MOTHERBOARD INFO]: \n" << "\tNAME: " << m_motherboard.getName() << "\n" << "\tCOMPANY: " << m_motherboard.getCompany() << "\n"
+         << "\tVIDEOSLOT: " << m_motherboard.getVideoSlot() << "\n" << "\tMEMORYSLOT: " << m_motherboard.getMemorySlot() << "\n"
+         << "\tPRICE: " << m_motherboard.getPrice() << "\n" << "\tSOCKET: " << m_motherboard.getSocket() << "\n";
+
+    cout << "Price for all: " << priceAll() << " UAH" << "\n";
 }
 
